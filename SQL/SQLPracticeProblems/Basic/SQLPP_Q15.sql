6509007a9cd135a6a05e05699ac93924d1e533a9
Seungjae Han <henry7646@gmail.com>
2020-01-24 08:28:28 +0900

    Add files via upload

---
+++ SQL/SQLPracticeProblems/Basic/SQLPP_Q15.sql
@@ -0,0 +1,5 @@
+SELECT min(orderdate) AS firstdate
+FROM orders;
+
+-- Need to be familiar with how to use the aggregate function
+-- Make sure what the difference between WHERE clause and HAVING clause is: should be familiar with their uses later
