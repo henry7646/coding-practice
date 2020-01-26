4ac05ee26672a01c50b36aa4c343894b70b8ccf4
henry7646 <henry7646@gmail.com>
2020-01-26 08:35:36 +0800

    Update SQLPP_Q19.sql

--- SQL/SQLPracticeProblems/Basic/SQLPP_Q19.sql
+++ SQL/SQLPracticeProblems/Basic/SQLPP_Q19.sql
@@ -4,3 +4,4 @@ WHERE orders.shipvia=shippers.shipperid AND orders.orderid < 10270
 ORDER BY orders.orderid;
 
 -- Do not make trivial mistakes such as putting '&' in the place of 'AND'
+-- Need to review how to conduct various types of joins (Equijoin, Left Outer join, Right Outer join, etc.) soon
