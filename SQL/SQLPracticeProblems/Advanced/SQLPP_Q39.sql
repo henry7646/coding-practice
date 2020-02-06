ec53d370e6663d649d1725dc210f9bd926702fd2
Seungjae Han <henry7646@gmail.com>
2020-02-06 21:30:18 +0900

    Update SQLPP_Q39.sql

--- SQL/SQLPracticeProblems/Advanced/SQLPP_Q39.sql
+++ SQL/SQLPracticeProblems/Advanced/SQLPP_Q39.sql
@@ -6,14 +6,6 @@ GROUP BY orderid, quantity
 HAVING COUNT(quantity) = 2);
 
 /*Instead of plugging the subquery, you can use the technique called CTE (Common Table Expression): it works for MySQL and Oracle, too
-SELECT * FROM orderdetails
-WHERE orderid IN
-(SELECT DISTINCT orderid FROM orderdetails
-WHERE quantity >=60
-GROUP BY orderid, quantity
-HAVING COUNT(quantity) = 2);
-
-/*Instead of plugging the subquery, you can use the technique called CTE (Common Table Expression): it works for MySQL and Oracle, too
 https://docs.microsoft.com/en-us/sql/t-sql/queries/with-common-table-expression-transact-sql?view=sql-server-ver15#examples*/
 WITH possiblywrongorders AS
 (SELECT DISTINCT orderid FROM orderdetails
