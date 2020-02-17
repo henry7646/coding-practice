cd96568aefb600c4e836a0c4b3092ede29259a58
Seungjae Han <henry7646@gmail.com>
2020-02-17 12:49:00 +0900

    Update Import_MySQL_Practice.sql
    1. Changed the data type of deliveryid from int to bigint
    2. Deleted the caveat on LOAD DATA LOCAL INFIELD .... not working

--- SQL/SQLD/IMPORT/Import_MySQL_Practice.sql
+++ SQL/SQLD/IMPORT/Import_MySQL_Practice.sql
@@ -1,8 +1,11 @@
 TRUNCATE TABLE deliveryinfo;
 DROP TABLE deliveryinfo;
 
+/*Caveat: If the row of the csv file to import is outside the range of -2^31(-2147483648) to 2^31-1(2147483647),
+then you need to set its data type as bigint, not int - 
+https://docs.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql?view=sql-server-ver15*/
 CREATE TABLE deliveryinfo (
-deliveryid int,
+deliveryid bigint,
 customerid int,
 deliverydate datetime,
 deliverycost int);
@@ -16,11 +19,3 @@ INTO TABLE deliveryinfo
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS
 (deliveryid, customerid, deliverydate, deliverycost);
-
-  
-/*Strangely enough, executing the SQL code above keeps on spitting out the error (Error Code: 1148. The used command is not allowed with 
-this MySQL version), at least for MySQL Workbench 8.0.18. There are multiple solutions offered online for this problem 
-(https://stackoverflow.com/questions/10762239/mysql-enable-load-data-local-infile), but none of them seem to work universally ever since 
-the problem has been raised for MySQL Workbench 8.0.12 (https://bugs.mysql.com/bug.php?id=91891). Hence, I highly recommend that you use 
-Table Data Import Wizard, which is intrduced in github.com/henry7646/coding-practice/SQL/SQLD/IMPORT/How to Import CSV File into MySQL.md*/  
-    
